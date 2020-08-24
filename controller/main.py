b31b705fdbc28e5042d23828d2a891265ea20b14
FiendChain <formulating.response@gmail.com>
2020-08-24 14:42:57 +1000

    Autodetect local ip

--- controller/main.py
+++ controller/main.py
@@ -3,44 +3,49 @@ from listen_socket import socket_listen
 from threading import Thread
 import argparse
 import websockets
+import socket
 import asyncio
 import time
 
 def website_thread(url):
-    print("Starting server")
+    print("running website")
     # flask_app.run(host=url, port=3000, debug=False, ssl_context=('localhost.crt', 'localhost.key'))
     flask_app.run(host=url, port=3000, debug=False)
 
-def socket_thread(url):
+def socket_thread(url, loop):
+    asyncio.set_event_loop(loop)
     server = websockets.serve(socket_listen, url, 8765)
-    asyncio.get_event_loop().run_until_complete(server)
-
-    try:
-        print("Running client")
-        asyncio.get_event_loop().run_forever()
-    except KeyboardInterrupt:
-        print("Stopping listener")
+    print("running websocket")
+    loop.run_until_complete(server)
+    loop.run_forever()
 
 def main():
+    DEFAULT_URL = socket.gethostbyname(socket.gethostname())
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="192.168.2.10")
+    parser.add_argument("--host", default=DEFAULT_URL)
     args = parser.parse_args()
 
     url = args.host
 
-    t1 = Thread(target=website_thread, args=(url,))
-    # t2 = Thread(target=socket_thread, args=(url,))
+    print(f"starting on {url}")
+
+    loop = asyncio.get_event_loop()
+
+    t1 = Thread(target=website_thread, args=(url,), daemon=True)
+    t2 = Thread(target=socket_thread, args=(url,loop), daemon=True)
 
     t1.start()
-    # t2.start()
+    t2.start()
 
     try:
-        socket_thread(url)
+        while True:
+            time.sleep(0.5)
     except KeyboardInterrupt:
-        print("Stopping")
+        print("stopping")
+        loop.stop()
     finally:
-        t1.join()
-        # t2.join()
+        print("closing threads")
 
 if __name__ == '__main__':
     main()
